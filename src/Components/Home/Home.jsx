0b90714fd617a4db775de6a5193383597610d899
Sojib Islam <sojibislam6505@gmail.com>
2025-11-20 02:14:18 +0600

    router introduction

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div><h1>
+      Home is here</h1></div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import About from './Components/About/About.jsx'
 import Home from './Components/Home/Home.jsx'
